d30f12f9a51f9ea99ad7f9dac3c7b85920633600
Christine Montcheu (Student) <N00230920@iadt.ie>
2024-10-21 12:02:57 +0100

    first commit

---
+++ database/migrations/2024_10_15_145159_create_shows_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shows', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('overview')->nullable();
+            $table->string('where_to_watch');
+            $table->integer('number_of_episodes');
+            $table->integer('air_date');
+            $table->integer('end_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shows');
+    }
+};
